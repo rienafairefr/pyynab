e4a1abab172e2a60e7fba01be108a8616b8f5fa4
Unknown <rienafairefr@gmail.com>
2019-02-03 09:27:50 +0100

    first commit, w swagger, Makefile etc

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # pyynab
-Library to connnect to the You neeed a Budget Public API
+Library to connnect to the You neeed a Budget Public API.
+
+
+It uses the public Swagger file [here](https://api.youneedabudget.com/papi/spec-v1-swagger.json)
+and [openapi-generator](https://github.com/OpenAPITools/openapi-generator/)
+
+In the swagger file, some parameters accept `null` asvalues but are not marked as nullable,
+to te Swagger spec needed some pre-processin: First it is converted to OpenApi 3, then patched with `path_openapi.py`
+, then a `python` client is generated using openapi-generator v3 
+
+See the doc of the auto-generated package in its own README.md
---
+++ patch_openapi.py
@@ -0,0 +1,39 @@
+import yaml
+
+
+def main():
+    with open('openapi.yaml', 'r') as openapi:
+        spec = yaml.load(openapi)
+
+    for k, v in spec['components']['schemas'].items():
+        for np, p in v.get('properties', {}).items():
+            pass
+
+    spec['components']['schemas']['Account']['properties']['note']['nullable'] = True
+    spec['components']['schemas']['Category']['properties']['note']['nullable'] = True
+    spec['components']['schemas']['MonthSummary']['properties']['note']['nullable'] = True
+    spec['components']['schemas']['Category']['properties']['goal_creation_month']['nullable'] = True
+    spec['components']['schemas']['Category']['properties']['goal_target_month']['nullable'] = True
+    spec['components']['schemas']['Payee']['properties']['transfer_account_id']['nullable'] = True
+    spec['components']['schemas']['TransactionSummary']['properties']['category_id']['nullable'] = True
+    spec['components']['schemas']['TransactionSummary']['properties']['flag_color']['nullable'] = True
+    spec['components']['schemas']['TransactionSummary']['properties']['flag_color'].pop('enum', None)
+    spec['components']['schemas']['TransactionSummary']['properties']['import_id']['nullable'] = True
+    spec['components']['schemas']['TransactionSummary']['properties']['memo']['nullable'] = True
+    spec['components']['schemas']['TransactionSummary']['properties']['payee_id']['nullable'] = True
+    spec['components']['schemas']['TransactionSummary']['properties']['matched_transaction_id']['nullable'] = True
+    spec['components']['schemas']['TransactionSummary']['properties']['transfer_account_id']['nullable'] = True
+    spec['components']['schemas']['TransactionSummary']['properties']['transfer_transaction_id']['nullable'] = True
+    spec['components']['schemas']['MonthDetail']['allOf'][1]['properties']['categories']['nullable'] = True
+    try:
+        spec['components']['schemas']['Category']['required'].remove('goal_type')
+    except:
+        pass
+    spec['components']['schemas']['Category']['properties']['goal_type'].pop('enum', None)
+
+    with open('openapi.yaml', 'w') as openapi:
+        yaml.safe_dump(spec, openapi, indent=2)
+
+
+if __name__ == '__main__':
+    main()
